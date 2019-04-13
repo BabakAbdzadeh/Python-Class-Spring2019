d5be86e315e9f9844e3aab5c293e7bdf87352cb9
BabakAbdzadeh <Babak.abdzadeh@gmail.com>
2019-04-13 16:33:20 +0430

    Hello world we a little fight!

---
+++ Hello World!.py
@@ -0,0 +1,55 @@
+import time
+firstper_string00 = "I am black board"
+secondper_string00 = "Hello World!I am black board too"
+
+firstper_string01 = "nice too meet you black board"
+secondper_string01 = "nice too meet you too"
+
+firstper_string02 = "is it your first time coding python?"
+secondper_string02 = " yeah! its my first time ,I am almost a noob"
+
+firstper_string03 =  "you are definitely a noob"
+secondper_string03 = "don't insult me please,man you are a jerk!"
+
+firstper_string04 = "hahahahaha"
+secondper_string04 = "I'll kick your A**!You SOB!"
+
+
+print(firstper_string00)
+
+
+time.sleep(1)
+print(secondper_string00)
+
+
+time.sleep(1.5)
+
+print(firstper_string01)
+
+time.sleep(1)
+
+print(secondper_string01)
+
+time.sleep(1.5)
+
+print(firstper_string02)
+
+time.sleep(1)
+
+print(secondper_string02)
+
+time.sleep(1.5)
+
+print(firstper_string03)
+
+time.sleep(1)
+
+print(secondper_string03)
+
+time.sleep(1.5)
+
+print(firstper_string04)
+
+time.sleep(1)
+
+print(secondper_string04)
