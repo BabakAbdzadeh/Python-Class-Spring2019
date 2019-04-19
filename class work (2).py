d21ef2d25c2772dd8fd5d2ac2301e0713a0a0989
BabakAbdzadeh <Babak.abdzadeh@gmail.com>
2019-04-19 15:06:02 +0430

    class session 2

---
+++ class work (2).py
@@ -0,0 +1,26 @@
+# list
+
+# using ( [] ) for define "list"
+# example :
+# *mylist = [10 , 'hello' , 10.2 ]
+# this "list" can include all types
+
+# searching in list
+# example :
+# *mylist[0]
+# *print(mylist[0]) #will print first member of the list
+# *type(mylist[2])  #will show the type of the member of the list
+# *len(mylist) #will show us the length of the list
+# example
+# *length_of_my_list = len(mylist) #save the length of our list in a variable
+
+
+# test
+# use sum and length to calculate the sum and average of a variable numbers = [1,2,3,4,5,6,7,8]
+number = [1,2,3,4,5,6,7,8]
+sum_number =sum(number)
+length_number = len(number)
+average_number = sum_number / length_number
+
+print(sum_number)
+print(average_number)
