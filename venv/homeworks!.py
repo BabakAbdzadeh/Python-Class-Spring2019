09218dfc34f43d49725b8c4697fe4b86a86332cd
BabakAbdzadeh <Babak.abdzadeh@gmail.com>
2019-04-20 16:52:44 +0430

    home work 1.1

--- venv/homeworks!.py
+++ venv/homeworks!.py
@@ -4,5 +4,19 @@
 
 mass_var = float(input("Please enter your weight in kg: "))
 height_var = float(input("please enter you height in meter: "))
+# extra work for stupid people who write their height in Cm
+if height_var > 3:
+    height_var = height_var / 100
+    if height_var > 3:
+        Print("get the hell out of here you idiot!")
+
 bmi_var = float(mass_var / (height_var * height_var))
-print(bmi_var)
+
+if bmi_var > 30:
+    print("your bmi is :  ", bmi_var, " and its  'Obesity' ")
+elif 25 < bmi_var <= 30:
+    print("your BMI is:", bmi_var, " and its 'Overweight'")
+elif 18.5 < bmi_var <= 25:
+    print("your BMI is:", bmi_var, " and its 'Normal'")
+else:
+    print("your BMI is:", bmi_var, " and its 'Underweight'")
