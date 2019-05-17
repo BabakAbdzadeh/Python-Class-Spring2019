fd627c1201a6a07392a808278869dc11f990c6c4
BabakAbdzadeh <Babak.abdzadeh@gmail.com>
2019-05-17 17:04:04 +0430

    Quiz working

--- Class , Q by teacher.py
+++ Class , Q by teacher.py
@@ -1,12 +1,12 @@
 def quiz(input_string):
     i = 0
 
-    string_output = ""
+    output_string = ""
     while i != len(input_string):
-        i = 0
-        ascii_input = ord(input_string(i))
-        if ascii_input != 32 or ascii_input != 121 or ascii_input != 122:
-            ascii_1char_out_put = ascii_input + 2
+
+        ascii_input = ord(input_string[i])
+        if int(ascii_input) != 32 or int(ascii_input) != 121 or int(ascii_input) != 122:
+            ascii_1char_out_put = int(ascii_input) + 2
 
         if ascii_input == 121:
             ascii_1char_out_put += 1
@@ -16,11 +16,11 @@ def quiz(input_string):
             ascii_1char_out_put == ascii_input
 
         char_out_put = chr(ascii_1char_out_put)
-        string_output = string_output + char_out_put
+        output_string = output_string + char_out_put
         # string_output_list = list(string_output)
         i += 1
 
-    return string_output
+    return output_string
 
 
-print(quiz("a"))
+print(quiz("abcz"))
