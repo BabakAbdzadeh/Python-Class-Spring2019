4d1dafa3cd9ff7204c22655649ff22bc0df52465
BabakAbdzadeh <Babak.abdzadeh@gmail.com>
2019-05-28 20:31:34 +0430

    1. creation duration (with function)

--- HomeWorks/Etefagh.py
+++ HomeWorks/Etefagh.py
@@ -1,8 +1,15 @@
 data = [50.3, 338.4, 626.5, 959.4, 1317.9, 1716.7, 2134.3, 2565.5, 3085.6, 3626.7]
-duration = []
-i = 0
-while i+1 != len(data):
-    duration_parameter = data[i+1] - data[i]
-    duration.append(duration_parameter)
-    i += 1
+
+
+def duration_calculator(data_in):
+    duration_calc = []
+    i = 0
+    while i + 1 != len(data_in):
+        duration_parameter = data_in[i + 1] - data_in[i]
+        duration_calc.append(duration_parameter)
+        i += 1
+    return duration_calc
+
+
+duration = duration_calculator(data)
 print(duration)
